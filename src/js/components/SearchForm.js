d9ffc4030a05a779ceff644cfff988530c68320f
Milos Zarkovic <milos.zarkovic@kupujemprodajem.com>
2018-03-29 16:20:43 +0200

    remove AppStore from SearchForm component

--- src/js/components/SearchForm.js
+++ src/js/components/SearchForm.js
@@ -1,7 +1,6 @@
 const React = require('react');
 
 const AppActions = require('../actions/AppActions');
-const AppStore = require('../stores/AppStore');
 
 const SearchForm = React.createClass({
     render: function () {
