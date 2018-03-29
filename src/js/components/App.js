abd83803c01a345f65110a97a84cd549049974b3
Milos Zarkovic <milos.zarkovic@kupujemprodajem.com>
2018-03-29 12:06:00 +0200

    main App component and AppDispatcher

---
+++ src/js/components/App.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+const AppActions = require('../actions/AppActions');
+const AppStore = require('../stores/AppStore');
+
+const App = React.createClass({
+    render: function () {
+        return (
+            <div>My app</div>
+        );
+    }
+});
+
+module.exports = App;
